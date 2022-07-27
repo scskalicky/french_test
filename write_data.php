9cc1af502e7670cb5f688bd672d80bd3140890a4
Stephen Skalicky <scskalicky@gmail.com>
2022-07-28 11:31:33 +1200

    tweaking php

--- write_data.php
+++ write_data.php
@@ -6,7 +6,7 @@ $id = $post_data['ID'];
 // generate a unique ID for the file, e.g., session-6feu833950202 
 $file = uniqid("session-");
 // the directory "data" must be writable by the server
-$name = "data/{$file$id}.csv"; 
+$name = "data/{$file}{$id}.csv"; 
 // write the file to disk
 file_put_contents($name, $data);
 ?>
