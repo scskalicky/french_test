5566a69ad834055419a6c06f4c7feeba1e4b4446
Stephen Skalicky <scskalicky@gmail.com>
2022-07-28 14:06:28 +1200

    dd

--- save_audio.php
+++ save_audio.php
@@ -8,6 +8,7 @@ $audio_id = uniqid("audio-");
 // save the data as a .txt file to server with audio id
 file_put_contents("data/" . $audio_id . ".txt", $data);
 
+$audio_return['audio_id'] = $audio_id;
 // echo the audio id back to jsPsych to be added to data as a replacement for the audio
-echo($audio_id);
+echo json_encode($audio_return)
 ?>
