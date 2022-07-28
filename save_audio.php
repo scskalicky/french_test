7df7015676c5fc639425c9e483a8a199dd1ff7d0
Stephen Skalicky <scskalicky@gmail.com>
2022-07-28 12:37:55 +1200

    Update save_audio.php

--- save_audio.php
+++ save_audio.php
@@ -1,6 +1,8 @@
 <?php
-
-$audio = $_POST['audio_base64'];
-$audio_id = uniqid("audio-")
+$post_data = json_decode(file_get_contents('php://input'), true);
+$data = $post_data['audio_base64'];
+$audio_id = uniqid("audio-");
+$name = 'data/{$audio_id}.txt';
+file_put_contents($name, $data)
 echo $audio_id;
 ?>
