3cdf4fa5e7c72896ba31f0cd39b02c50959c380e
Stephen Skalicky <scskalicky@gmail.com>
2022-07-28 17:58:25 +1200

    Update audio_stimuli.js

--- audio_stimuli.js
+++ audio_stimuli.js
@@ -1,3 +1,3 @@
-var audio_stimuli = [
+audio_stimuli = [
     {turn1: 'audio/fiona1.mp3', turn2: 'audio/fiona2.mp3', turn3: 'audio/fiona3.mp3', data: {stim_id: 'fiona'}},{turn1: 'audio/torin1.mp3', turn2: 'audio/torin2.mp3', turn3: 'audio/torin3.mp3', data: {stim_id: 'torin'}}
 ]
