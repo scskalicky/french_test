bc0c6efc5722294101fc5e40380365cd50de16d3
Stephen Skalicky <scskalicky@gmail.com>
2022-07-28 13:17:44 +1200

    Update save_audio.php

--- save_audio.php
+++ save_audio.php
@@ -5,7 +5,7 @@ $audio_id = uniqid("audio-");
 
 $name = "data/{$audio_id}.txt";
 
-file_put_contents($audio_id . "txt", $data);
+file_put_contents("data/" . $audio_id . ".txt", $data);
 
 echo json_encode($audio_id);
 ?>
