11bd15f120ae309db042bcf115db63eba3397799
Stephen Skalicky <scskalicky@gmail.com>
2022-07-28 12:41:45 +1200

    audio saving php

--- save_audio.php
+++ save_audio.php
@@ -2,7 +2,10 @@
 $post_data = json_decode(file_get_contents('php://input'), true);
 $data = $post_data['audio_base64'];
 $audio_id = uniqid("audio-");
+
 $name = 'data/{$audio_id}.txt';
-file_put_contents($name, $data)
+
+file_put_contents($name, $data);
+
 echo $audio_id;
 ?>
