68c2a1a7bd5b5c01dca02a750d0bd376ad421e3f
Stephen Skalicky <scskalicky@gmail.com>
2022-07-28 15:55:51 +1200

    dev

--- save_audio.php
+++ save_audio.php
@@ -1,13 +1,12 @@
 <?php
 // grab the data from the post call
 $data = $_POST['audio_base64'];
-$trial = $_POST['trial'];
-$subject = $_POST['subject'];
+
 // create a random audio id
 $audio_id = uniqid("audio-");
 
 // save the data as a .txt file to server with audio id
-file_put_contents("data/" $audio_id . ".txt", $data);
+file_put_contents("data/" . $audio_id . ".txt", $data);
 
 //$audio_return['audio_id'] = $audio_id;
 // echo the audio id back to jsPsych to be added to data as a replacement for the audio
