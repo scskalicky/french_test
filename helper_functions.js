8e90754c13f04a967f4f97ddda3731b969cb9dc3
Stephen Skalicky <scskalicky@gmail.com>
2022-07-31 11:28:35 +1200

    dev

--- helper_functions.js
+++ helper_functions.js
@@ -3,18 +3,17 @@
 // creates a random audio id based on the username + date + random number
 // pushes the audio data to server then overwrites audio data from the jsPsych data file
 // one thing to think about is perhaps creating subfolders for each participant. 
-function purgeAudio(data){
+function purgeAudio(subject, data){
     // create a random id for the specific audio file.
-    subject = jsPsych.data.get().select('subject')[0]
+    // subject = jsPsych.data.get().select('subject')[0]
     audio_id = (subject).toString().concat('_audio-').concat(Date.now().toString().concat('_').concat(Math.floor(Math.random()*9999)).toString())
        console.log(audio_id);
        // call the php script
        $.ajax ({
-                         url: "save_audio.php",
-                         type: "POST",
-                         data: {audio_base64: data.response, identifier: audio_id, sub: subject},
-             dataType: 'text',
-           });
+            url: "save_audio.php",
+            type: "POST",
+            data: {audio_base64: data.response, identifier: audio_id, sub: subject},
+            dataType: 'text'});
       // replace the base64 in the jsP data with the audio_id
      data.response = audio_id;
  }
