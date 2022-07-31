6abd2a902f4463163fdd35e967c458e007c6ae6b
Stephen Skalicky <scskalicky@gmail.com>
2022-07-31 17:45:44 +1200

    dev

--- helper_functions.js
+++ helper_functions.js
@@ -25,6 +25,6 @@ function saveData(name, data){
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'write_data.php');
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({filedata: data}));
+    xhr.send(JSON.stringify({filedata: data, subject:name}));
   }
   
--- write_data.php
+++ write_data.php
@@ -2,10 +2,11 @@
 // get the data from the POST message
 $post_data = json_decode(file_get_contents('php://input'), true);
 $data = $post_data['filedata'];
+$subjet = $post_data['subject']
 // generate a unique ID for the file, e.g., session-6feu833950202 
 $file = uniqid("session-");
 // the directory "data" must be writable by the server
-$name = "data/{$file}.csv"; 
+$name = "data/{$subject}/{$file}.csv"; 
 // write the file to disk
 file_put_contents($name, $data);
 ?>
