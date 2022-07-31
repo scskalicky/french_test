008b7f6bb7f2921546530b6a8afbb51838a8988e
Stephen Skalicky <scskalicky@gmail.com>
2022-07-31 14:39:58 +1200

    dev

--- conversation_trials.js
+++ conversation_trials.js
@@ -1,25 +1,3 @@
 
 //jsPsych = initJsPsych()
 
-let play_audio_button = {
-    type: jsPsychHtmlButtonResponse,
-    stimulus: '',
-    choices: ['PLAY AUDIO']
-}
-
-let record_response_button = {
-    type: jsPsychHtmlButtonResponse,
-    stimulus: '',
-  choices: ['RECORD RESPONSE']
-}
-
-let record_answer = {
-    type: jsPsychHtmlAudioResponse,
-    stimulus: 'RECORDING...',
-    recording_duration: 5000,
-    show_done_button: true,
-    done_button_label: "Finish Recording",
-    // save audio
-    on_finish: function(data){
-      purgeAudio(jsPsych.data.get().select('subject').values[0].Q0,jsPsych.data.get().last(3).values()[0].stimulus, data)
-}}
