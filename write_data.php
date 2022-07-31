620a93f5dfb8cf9d2be465a6d997efa68e0cc588
Stephen Skalicky <scskalicky@gmail.com>
2022-07-31 17:58:36 +1200

    dev

--- write_data.php
+++ write_data.php
@@ -1,12 +1,17 @@
 <?php
 // get the data from the POST message
 $post_data = json_decode(file_get_contents('php://input'), true);
+
 $data = $post_data['filedata'];
-$subject = $post_data['subject'];
+
+$subject = $data.subject
+
 // generate a unique ID for the file, e.g., session-6feu833950202 
 $file = uniqid("session-");
+
 // the directory "data" must be writable by the server
 $name = "data/{$subject}/{$file}.csv"; 
+
 // write the file to disk
 file_put_contents($name, $data);
 ?>
