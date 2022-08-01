3b25a5b1e959dcb405317dab86947ae719f0f361
Stephen Skalicky <scskalicky@gmail.com>
2022-08-02 08:45:46 +1200

    tweak convert py

--- convert_audio.py
+++ convert_audio.py
@@ -1,17 +1,19 @@
 import base64
 import glob
+from ffmpy import FFmpeg
 
 # how to convert base64 to audio
-files = glob.glob('full_send/*.txt')
-
 def convertAudio(filename):
     decoded = base64.b64decode(open(filename).read())
     new_filename = filename.split('.')[0] + '.webm'
+    print(new_filename[10:])
     with open(new_filename, 'wb') as w:
+        print(new_filename)
         w.write(decoded)
     
     converted_filename = new_filename.split('.')[0] + '.wav'
 
+    # will exit with error if file already exists.
     ff = FFmpeg(
         executable = '/usr/local/bin/ffmpeg',
         inputs = {new_filename: None},
@@ -19,6 +21,10 @@ def convertAudio(filename):
     ff.cmd
     ff.run()
 
+folders = glob.glob('raw_audio/*')
 
-for file in files:
-    convertAudio(file)
+for folder in folders:
+    files = glob.glob(folder + "/" +  "*.txt")
+    print(files)
+    for file in files:
+        convertAudio(file)
