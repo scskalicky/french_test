db4e36b7b4b59e258d42d14bb0e9d72ac7181760
Stephen Skalicky <scskalicky@gmail.com>
2022-08-04 17:06:46 +1200

    Update plugin-cloze.js

--- jspsych/plugin-cloze.js
+++ jspsych/plugin-cloze.js
@@ -52,7 +52,7 @@ var jsPsychCloze = (function (jspsych) {
                   html += elements[i];
               }
               else {
-                  html += `<input type="text" id="input${solution_counter}" value="">`;
+                  html += `<input type="text" style="width:1px "id="input${solution_counter}" value="">`;
                   solution_counter++;
               }
           }
