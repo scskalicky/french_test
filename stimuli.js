38ea70685118bfd667aa5869fb69bfe1dc50957b
Stephen Skalicky <scskalicky@gmail.com>
2022-08-05 15:43:49 +1200

    Update stimuli.js

--- stimuli.js
+++ stimuli.js
@@ -10,7 +10,7 @@ var baf_stim = [
     data: {stim_id: 'torin'}}
 ];
 
-var french_characters = 'lettres accentuées:<br> [ç, &eacute;, &acirc;/&ecirc;/&icirc;/&ocirc;/&ucirc;, &agrave;/&egrave;/&igrave;/&ograve;/&ugrave;, &euml;/&iuml;/&uuml;]<br><br>'
+var french_characters = '&Agrave;	&#192;	&#xC0;	lettres accentuées:<br> [ç, &eacute;, &acirc;/&ecirc;/&icirc;/&ocirc;/&ucirc;, &agrave;/&egrave;/&igrave;/&ograve;/&ugrave;, &euml;/&iuml;/&uuml;]<br><br>'
 
 var c_test_stim = [
     {c_text: french_characters.concat("Coucou, tu es d'accord pour m'aider &agrave; organiser l'anniversaire de Clara? Hier, no%% avons pr&eacute;%% un gr%% g&acirc;teau a%% chocolat. M%% s&oelig;urs o%% achet&eacute; d%% d&eacute;corations e%% des bon%% hier mi%%. Tu pe%% prendre d%% boissons e%% de l%% glace. L%% f&ecirc;te e%% samedi, ch%% moi.  Vi%% av%% ton co%% de fran%%, on v%% faire u%% salade d%% fruits ense%%.  Apr&egrave;s o%% va pr&eacute;p%% la mai%% et met%% de l%% musique. Appelle-moi quand tu arrives."), data: {stim_id: 'ctest_text1'}}
