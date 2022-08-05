9349c4f6ff1b0e5271cedeb530a8b5e993885f87
Stephen Skalicky <scskalicky@gmail.com>
2022-08-05 15:17:17 +1200

    dev

--- stimuli.js
+++ stimuli.js
@@ -1,4 +1,4 @@
-const baf_stim = [
+var baf_stim = [
     {turn1: 'audio/fiona1.mp3', 
     turn2: 'audio/fiona2.mp3', 
     turn3: 'audio/fiona3.mp3', 
@@ -10,5 +10,8 @@ const baf_stim = [
     data: {stim_id: 'torin'}}
 ];
 
+var french_characters = 'lettres accentuées:<br> [ç, é, â/ê/î/ô/û, à/è/ì/ò/ù, ë/ï/ü]'
 
-
+var c_test_stim = [
+    {c_text: "Coucou, tu es d'accord pour m'aider à organiser l'anniversaire de Clara? Hier, no%% avons pré%% un gr%% gâteau a%% chocolat. M%% sœurs o%% acheté d%% décorations e%% des bon%% hier mi%%. Tu pe%% prendre d%% boissons e%% de l%% glace. L%% fête e%% samedi, ch%% moi.  Vi%% av%% ton co%% de fran%%, on v%% faire u%% salade d%% fruits ense%%.  Après o%% va prép%% la mai%% et met%% de l%% musique. Appelle-moi quand tu arrives.", data: {stim_id: 'ctest_text1'}}
+]
